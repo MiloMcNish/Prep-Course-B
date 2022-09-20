26a57baf75872b75e94662a9bb0864b1d77233c2
MiloMcNish <100816914+MiloMcNish@users.noreply.github.com>
2022-09-19 22:06:55 -0500

    Homework 3

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,31 +8,57 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var acc = 0;
+    for (var i = 1; i<11; i++) {
+      acc = acc + i;
+      }
+      return acc
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var pares = [];
+  for (var i = 0; i<array.length; i++) {
+    if (array[i]%2 === 0) {
+      
+      pares.push(array[i]);
+    } else {
+
+    }
+    
+  }
+  return pares
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  return array.map(function(i){
+    return i**2;
+  })
+  
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  return array.reduce(function(acc,i){
+    return acc+i;
+  })
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return num.toString().length; 
 }
-  
+
+
+
   
   // No modificar nada debajo de esta línea
   // --------------------------------
